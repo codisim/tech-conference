d48e694a04f19ec5aed9674d59a0d4dc79c59b7a
MD Waliullah <waliullah.godev@gmail.com>
2026-03-12 22:45:35 +0600

    refactor: swagger

--- src/modules/venues/venues.controller.ts
+++ src/modules/venues/venues.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { VenuesService } from './venues.service';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RoleGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/role.decorators';
@@ -20,7 +20,30 @@ export class VenuesController {
     @UseGuards(JwtAuthGuard, RoleGuard)
     @Roles(UserRole.ADMIN)
     @ApiBearerAuth('JWT-auth')
-    @ApiOperation({ summary: 'Create a new venue' })
+    @ApiOperation({
+        summary: 'Create a new venue',
+        description: 'Only admins can create a new venue'
+    })
+
+    @ApiResponse({
+        status: 201,
+        description: 'Venue created successfully',
+        type: VenueResponseDto
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'Unauthorized'
+    })
+    @ApiResponse({
+        status: 403,
+        description: 'Forbidden'
+    })
+
+    @ApiResponse({
+        status: 500,
+        description: 'Internal Server Error'
+    })
+
     @ApiBody({ type: CreateVenueDto })
     async createVenue(@Body() createVenueDto: CreateVenueDto): Promise<VenueResponseDto> {
         return this.venuesService.createVenue(createVenueDto);
