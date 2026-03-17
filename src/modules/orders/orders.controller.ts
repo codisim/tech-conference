588010dbea9b56fe337094165d3f79c839d6d53b
MD Waliullah <waliullah.godev@gmail.com>
2026-03-18 05:20:03 +0600

    feat: create order

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { OrderResponseDto } from './dto/response-order.dto';
+import { CreateOrderDto } from './dto/create-order.dto';
 
+
+@ApiTags('Orders')
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+    constructor(private readonly ordersService: OrdersService) { }
+
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @ApiOperation({ summary: 'Create Order' })
+    @ApiResponse({
+        status: 201,
+        description: 'Order created successfully',
+        type: OrderResponseDto,
+    })
+
+    @ApiResponse({
+        status: 400,
+        description: 'Bad request',
+    })
+    @ApiResponse({
+        status: 500,
+        description: 'Internal server error',
+    })
+
+    create(@Body() createOrderDto: CreateOrderDto) {
+        return this.ordersService.create(createOrderDto);
+    }
+
+
+}
--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,4 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { OrderResponseDto } from './dto/response-order.dto';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor(
+        private readonly prisma: PrismaService
+    ) { }
+
+    // create
+    async create(createOrderDto: CreateOrderDto): Promise<OrderResponseDto> {
+        return this.prisma.order.create({
+            data: createOrderDto,
+            include: {
+                payment: true,
+                tickets: true,
+            },
+        });
+    }
+
+    async findAll(): Promise<OrderResponseDto[]> {
+        return this.prisma.order.findMany({
+            include: {
+                payment: true,
+                tickets: true,
+            },
+        });
+    }
+
+    async findOne(id: string): Promise<OrderResponseDto> {
+        const order = await this.prisma.order.findUnique({
+            where: { id },
+            include: {
+                payment: true,
+                tickets: true,
+            },
+        });
+
+        if (!order)
+            throw new NotFoundException('Order not found');
+
+        return order;
+    }
+
+
+    // update only admin
+    async updateStatus(id: string, status: any) {
+        await this.findOne(id);
+
+        return this.prisma.order.update({
+            where: { id },
+            data: { status },
+        });
+    }
+
+
+    // delete
+    async remove(id: string): Promise<{ message: string }> {
+        const existingOrder = await this.findOne(id);
+
+        if (!existingOrder)
+            throw new NotFoundException('Order not found');
+
+        await this.prisma.order.delete({
+            where: { id },
+        });
+
+        return { message: 'Order deleted successfully' };
+    }
+
+}
